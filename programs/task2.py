5f24f3c4ab1da0c604ee786c1548add2f85644f4
Harsh <i.harshparmar@gmail.com>
2020-09-02 15:12:57 -0400

    task2.py

---
+++ programs/task2.py
@@ -0,0 +1,7 @@
+'''
+                            TASK TWO: OPERATORS AND DECISION MAKING STATEMENT
+1.	Write a program in Python to perform the following operation:
+●	If a number is divisible by 3 it should print “Consultadd” as a string
+●	If a number is divisible by 5 it should print “c” as a string
+●	If a number is divisible by both 3 and 5 it should print “Consultadd Python Training” as a string.
+'''
