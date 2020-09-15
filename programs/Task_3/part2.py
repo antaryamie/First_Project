fda71567e6256a36af5087aac352fdc844c312c0
Antaryamie <i.harshparmar@gmail.com>
2020-09-14 23:04:03 -0400

    tasks update

---
+++ programs/Task_3/part2.py
@@ -0,0 +1,30 @@
+'''
+MORE QUESTIONS ON DATA STRUCTURES
+
+1.	Create a list of the 10 elements of four different types of Data Type like int, string, complex and float.
+2. 	Create a list of size 5 and execute the slicing structure 
+3. 	Create a list of given structure and run 
+	x=[100,200,300,400,500,[1,2,3,4,5,[10,20,30,40,50],6,7,8,9],600,700,800]
+Access list [1, 2, 3, 4]
+Access list [600,  700]
+Access list [100, 300, 500, 600, 800]
+Access list [[800, 700, 600, [1, 2, 3, 4, 5, [10, 20, 30, 40, 50], 6, 7, 8, 9], 500, 400, 300, 200, 100]]
+Access list [10]
+Access list [ ]
+4. 	Create a list of thousand number using range and xrange and see the difference between each other.
+5. 	How Tuple is beneficial as compare to the list?
+6. 	Write a program in Python to iterate through the list of numbers in the range of 1,100 and print the number which is divisible by 3 and a multiple of 2.
+7. 	Write a program in Python to reverse a string and print only the vowel alphabet if exist in the string with their index.
+8. 	Write a program in Python to iterate through the string “hello my name is abcde” and print the string which has even length of word.
+9. 	Write a program in python to print the pair of numbers whose sum is equal to result number that is let's say 8.
+x=[1,2,3,4,5,6,7,8,9,-1]
+10. 	Write a program in Python to complete the following task:
+Create two different list as in even_list and odd_list
+Ask user to enter the number in the range of 1,50 and make sure if the entered number is even append it to the even_list and if the entered number is odd append it to the odd list.
+Keep that in mind you can only add 5 items in each list
+Make sure once you entered the total 5 element calculate the sum of the list and return the maximum out of the list.
+11. 	Write a program to find out the occurrence of a specific word from an alphanumeric statement. Example: 12abcbacbaba344ab  
+                      Output: a=5 b=5 c=2 make sure you should avoid the numbers in you logic
+
+12.          Generate and print another tuple whose values are even numbers in the given tuple (1,2,3,4,5,6,7,8,9,10).
+'''
